d966bcf3b268df67c539d8a8bbc58106485d1a02
Bonnet <baptiste.bonnet4@orange.fr>
2020-10-26 20:40:08 +0100

    Ajout des posts et nombre de like

--- my-app-tp4/src/index.js
+++ my-app-tp4/src/index.js
@@ -6,9 +6,34 @@ import './index.css';
 
 class Post extends React.Component {
 
+  constructor(props){
+    super(props);
+  }
+
+  getLike(i){
+    this.props.value[i].like ++;
+    document.getElementById("postContainer"+i).innerHTML= this.props.value[i].like;
+  }
+
+  display_profils_posts()
+  {
+    const tab=[];
+    for(let i=0; i<this.props.value.length;i++) 
+    tab.push(
+      <div>
+        {this.props.value[i].contenu}
+        <button key = {i} onClick = {() => this.getLike(i)}>C'est super</button>
+        <span id={"postContainer"+i}>{this.props.value[i].like}</span>
+      </div>
+    );
+    return (<div>{tab}</div>);
+  }
+
 	render() {
 		return (
-      <div></div>
+      <div>
+        {this.display_profils_posts()}
+      </div>
 		);
 	}
 }
@@ -21,15 +46,14 @@ class Profil extends React.Component {
   }
 
   getColor(){
-    this.setState({
-      color : "rgb("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+")"
-    });
-
+    var myString = "rgb("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+")";
+    this.props.value.color = myString;
+    document.getElementById("profilContainer").style.backgroundColor= myString;
   }
 
   render() {
     return (
-      <div style={{ backgroundColor: this.state.color }}>
+      <div id="profilContainer" style={{ backgroundColor: this.props.value.color }}>
       {this.props.value.surname} <br/>
       <img src = {this.props.value.picture} height = "200" width = "200"></img>
       <button onClick ={() => this.getColor()}>Change Style</button>
@@ -50,6 +74,7 @@ class Page extends React.Component {
                 surname : "Léponge",
                 date : "01/05/1999",
                 picture : "bob.png",
+                color : 'Yellow',
                 posts : [{contenu : "J'adore les chips",like : 0},{contenu : "J'adore les petites filles, trop mimi",like : 0},{contenu : "Hier soir, je suis parti au parc astérix Trop le fun",like : 0}]
             },
             {
@@ -57,6 +82,7 @@ class Page extends React.Component {
                 surname : "Marlier",
                 date : "25/07/1954",
                 picture : "martine.png",
+                color : 'Blue',
                 posts : [{contenu : "Bonsoir tt le monde",like : 0},{contenu : "Je rejoins facebook version wish",like : 0},{contenu : "Trop bien",like : 0}]
             },
             {
@@ -64,6 +90,7 @@ class Page extends React.Component {
                 surname : "Lachenille",
                 date : "01/03/1994",
                 picture : "camille.png",
+                color : 'Green',
                 posts : [{contenu : "Je suis trop beau",like : 0},{contenu : "Je suis à dalas",like : 0},{contenu : "Les chiens c'est cool",like : 0}]
             }
           ],
@@ -97,6 +124,7 @@ class Page extends React.Component {
         {this.display_profils_button()}
 
         <Profil value = {this.state.profils[this.state.currentProfil]} />
+        <Post value = {this.state.profils[this.state.currentProfil].posts} />
 
       </div>
     );
