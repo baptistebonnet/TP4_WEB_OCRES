4dd6ec8f9252a05f8fee40460ae86d178cf81eb0
Bonnet <baptiste.bonnet4@orange.fr>
2020-10-26 19:42:03 +0100

    Changement de profil + couleur de fond

--- my-app-tp4/src/index.js
+++ my-app-tp4/src/index.js
@@ -2,56 +2,103 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-profils : [
-            {
-                prenom: "Bob",
-                nom : "Léponge",
-                dateN : "01/05/1999",
-                imageP : "jeanne.jpg",
-                posts : [{contenu : "J'adore les chips",like : 0},{contenu : "J'adore les petites filles, trop mimi",like : 0},{contenu : "Hier soir, je suis parti au parc astérix Trop le fun",like : 0}]
-            },
-            {
-                prenom: "Martine",
-                nom : "Marlier",
-                dateN : "25/07/1954",
-                imageP : "martine.jpg",
-                posts : [{contenu : "Bonsoir tt le monde",like : 0},{contenu : "Je rejoins facebook version wish",like : 0},{contenu : "Trop bien",like : 0}]
-            },
-            {
-                prenom: "Camille",
-                nom : "Lachenille",
-                dateN : "01/03/1994",
-                imageP : "camille.jpg",
-                posts : [{contenu : "Je suis trop beau",like : 0},{contenu : "Je suis à dalas",like : 0},{contenu : "Les chiens c'est cool",like : 0}]
-            }
-          ]
 
-
-
-class Profil extends React.Component {
- 
-  render() {
-    return (
-      
-    );
-  }
-}
 
 class Post extends React.Component {
 
 	render() {
 		return (
-
-		)
+      <div></div>
+		);
 	}
 }
 
+class Profil extends React.Component {
+ 
+  constructor(props){
+    super(props);
+    this.state = {color : 'Yellow'};
+  }
 
+  getColor(){
+    this.setState({
+      color : "rgb("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+")"
+    });
 
-class Page extends React.Component {
+  }
+
   render() {
     return (
-  
+      <div style={{ backgroundColor: this.state.color }}>
+      {this.props.value.surname} <br/>
+      <img src = {this.props.value.picture} height = "200" width = "200"></img>
+      <button onClick ={() => this.getColor()}>Change Style</button>
+      </div>
+    );
+  }
+}
+
+class Page extends React.Component {
+    
+    constructor(props) {
+
+    super(props);
+    this.state = { 
+      profils : [
+            {
+                firstname : "Bob",
+                surname : "Léponge",
+                date : "01/05/1999",
+                picture : "bob.png",
+                posts : [{contenu : "J'adore les chips",like : 0},{contenu : "J'adore les petites filles, trop mimi",like : 0},{contenu : "Hier soir, je suis parti au parc astérix Trop le fun",like : 0}]
+            },
+            {
+                firstname : "Martine",
+                surname : "Marlier",
+                date : "25/07/1954",
+                picture : "martine.png",
+                posts : [{contenu : "Bonsoir tt le monde",like : 0},{contenu : "Je rejoins facebook version wish",like : 0},{contenu : "Trop bien",like : 0}]
+            },
+            {
+                firstname : "Camille",
+                surname : "Lachenille",
+                date : "01/03/1994",
+                picture : "camille.png",
+                posts : [{contenu : "Je suis trop beau",like : 0},{contenu : "Je suis à dalas",like : 0},{contenu : "Les chiens c'est cool",like : 0}]
+            }
+          ],
+      currentProfil : 0
+    };
+
+  }
+
+  handleChange(i){
+    this.setState ({
+      currentProfil : i
+    });
+  }
+
+  display_profils_button()
+  {
+    const rendu=[];
+    for(let i=0; i<this.state.profils.length;i++) 
+    rendu.push(
+      <button key = {i} onClick = {() => this.handleChange(i)}>{this.state.profils[i].firstname}</button>
+    );
+    return (<div>{rendu}</div>);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Coucou
+        </p>
+
+        {this.display_profils_button()}
+
+        <Profil value = {this.state.profils[this.state.currentProfil]} />
+
+      </div>
     );
   }
 }
